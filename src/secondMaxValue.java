d16990ab25678f0a0028ea58241cf20b44b7eb6c
Hamza <149274544+Rokxany@users.noreply.github.com>
2024-11-08 02:30:25 +0500

    finding Second Max value

---
+++ src/secondMaxValue.java
@@ -0,0 +1,24 @@
+public class secondMaxValue{
+    public static void main(String[] args) {
+        int[] arr = {4,5,67,8,99};
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i] ;
+            } else if ( arr[i] >max2  && arr[i] != max1){
+                max2 = arr[i];
+            }
+        }
+        if (max2 == Integer.MIN_VALUE) {
+            System.out.println("There is no second value in the array");
+        }else{
+            System.out.println("The max value in the array is: "+ max1);
+            System.out.println("The Second max value in the array is: "+ max2);
+        }
+    }
+
+}
